4b154af0b49cbf190424217f2243f1642ae77e20
Daniel Goldstein <18djg@queensu.ca>
2020-10-01 22:00:18 -0400

    issue fix

--- MDP_Solver.py
+++ MDP_Solver.py
@@ -14,9 +14,7 @@ def stateValueFinder(matrix, actions, policy, discount, theta, specialPositions)
                 newStateValue = 0
                 for action in actions:
                     rewardAndLocation = determineRewardAndSPrimeLocation(action, i, j, rowLen, colLen, specialPositions)
-                    reward, sPrimeRow, sPrimeCol = rewardAndLocation[0], rewardAndLocation[1][0], rewardAndLocation[1][1]
-                    if (sPrimeRow, sPrimeCol) == specialPositions['A'][1]:
-                        print("OKAY ", sPrimeRow, sPrimeCol)
+                    reward, (sPrimeRow, sPrimeCol) = rewardAndLocation[0], rewardAndLocation[1]
                     newStateValue += policy * 1 * (reward + discount * matrix[sPrimeRow][sPrimeCol])
                 newMatrix[i][j] = newStateValue
                 delta = max(delta, abs(matrix[i][j] - newStateValue))
@@ -30,17 +28,17 @@ def determineRewardAndSPrimeLocation(action, row, col, rowLen, colLen, specialPo
     reward, location = 0, (row, col)
     if (row, col) == specialPositions['A'][0]:
         reward, location = 10, specialPositions['A'][1]
-    if (row, col) == specialPositions['B'][0]:
+    elif (row, col) == specialPositions['B'][0]:
         reward, location = 5, specialPositions['B'][1]
-    if action == 'LEFT' and col > 0:
+    elif action == 'LEFT' and col > 0:
         location = (row, col - 1)
-    if action == 'RIGHT' and col + 1 < colLen:
+    elif action == 'RIGHT' and col + 1 < colLen:
         location = (row, col + 1)
-    if action == 'UP' and row > 0:
+    elif action == 'UP' and row > 0:
         location = (row - 1, col)
-    if action == 'DOWN' and row + 1 < rowLen:
+    elif action == 'DOWN' and row + 1 < rowLen:
         location = (row + 1, col)
-    if row == location[0] and col == location[1] and reward == 0:
+    elif row == location[0] and col == location[1] and reward == 0:
         reward = -1
     return reward, location
 
@@ -62,28 +60,28 @@ def main():
     specialPositions7x7 = {'A': [(2, 1), (6, 1)], 'B': [(0, 5), (3, 5)]}
     m0 = [[0 for _ in range(5)] for _ in range(5)]
     m1 = [[0 for _ in range(7)] for _ in range(7)]
-   # m5x5_75 = stateValueFinder(m0, actions, policy, 0.75, theta, specialPositions5x5)
-   # m5x5_85 = stateValueFinder(m0, actions, policy, 0.85, theta, specialPositions5x5)
+    m5x5_75 = stateValueFinder(m0, actions, policy, 0.75, theta, specialPositions5x5)
+    m5x5_85 = stateValueFinder(m0, actions, policy, 0.85, theta, specialPositions5x5)
+    m7x7_75 = stateValueFinder(m1, actions, policy, 0.75, theta, specialPositions7x7)
+    m7x7_85 = stateValueFinder(m1, actions, policy, 0.85, theta, specialPositions7x7)
     book = stateValueFinder(m0, actions, policy, 0.9, theta, specialPositions5x5)
-   # m7x7_75 = stateValueFinder(m1, actions, policy, 0.75, theta, specialPositions7x7)
-   # m7x7_85 = stateValueFinder(m1, actions, policy, 0.85, theta, specialPositions7x7)
 
     print("State Value For 5x5, Gamma 0.75, Rounded To 1 Decimal Place")
-   # printMatrix(m5x5_75)
-   # print("\nNot rounded")
-  #  printMatrix(m5x5_75, roundMatrix=False)
+    printMatrix(m5x5_75)
+    print("\nNot rounded")
+    printMatrix(m5x5_75, roundMatrix=False)
     print("\nState Value For 5x5, Gamma 0.85, Rounded To 1 Decimal Place")
-   # printMatrix(m5x5_85)
-    #print("\nNot rounded")
-   # printMatrix(m5x5_85, roundMatrix=False)
+    printMatrix(m5x5_85)
+    print("\nNot rounded")
+    printMatrix(m5x5_85, roundMatrix=False)
     print("\nState Value For 7x7, Gamma 0.75, Rounded To 1 Decimal Place")
-   # printMatrix(m7x7_75)
-  #  print("\nNot rounded")
-  #  printMatrix(m7x7_75, roundMatrix=False)
+    printMatrix(m7x7_75)
+    print("\nNot rounded")
+    printMatrix(m7x7_75, roundMatrix=False)
     print("\nState Value For 7x7, Gamma 0.85, Rounded To 1 Decimal Place")
-   # printMatrix(m7x7_85)
-   # print("\nNot rounded")
-   # printMatrix(m7x7_85, roundMatrix=False)
+    printMatrix(m7x7_85)
+    print("\nNot rounded")
+    printMatrix(m7x7_85, roundMatrix=False)
     print("book\n")
     print(book)
     printMatrix(book)
