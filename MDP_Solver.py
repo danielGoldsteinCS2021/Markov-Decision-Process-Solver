2d1a0f2bbe99d5664c823204466fde8e0d6e4085
Daniel Goldstein <18djg@queensu.ca>
2020-10-01 23:43:36 -0400

    Solved

--- MDP_Solver.py
+++ MDP_Solver.py
@@ -2,7 +2,7 @@
 # 20119615
 # CISC 474 Assignment 1
 
-# stateValueFinder(2dArray, Array, Float, Float, Float, Dictionary):
+# stateValueFinder(2dArray, Array, Float, Float, Float, Dictionary) -> Matrix
 # stateValueFinder finds the state values for each entry in the matrix passed to it
 # it does so based on the actions, policy, discount, theta and specialPositions provided
 def stateValueFinder(matrix, actions, policy, discount, theta, specialPositions):
@@ -24,6 +24,8 @@ def stateValueFinder(matrix, actions, policy, discount, theta, specialPositions)
     return matrix
 
 
+# determineRewardAndSPrimeLocation(Array, Int, Int, Int, Int, Dictionary) -> Int, (Int, Int)
+# returns reward for provided action and the location the agent will be in after taking said action
 def determineRewardAndSPrimeLocation(action, row, col, rowLen, colLen, specialPositions):
     reward, location = 0, (row, col)
     if (row, col) == specialPositions['A'][0]:
@@ -38,11 +40,13 @@ def determineRewardAndSPrimeLocation(action, row, col, rowLen, colLen, specialPo
         location = (row - 1, col)
     elif action == 'DOWN' and row + 1 < rowLen:
         location = (row + 1, col)
-    elif row == location[0] and col == location[1] and reward == 0:
+    elif row == location[0] and col == location[1]:  # we will hit a wall if we move
         reward = -1
     return reward, location
 
 
+# printMatrix(2dArray, Boolean) -> None
+# prints provided matrix and rounds values if roundMatrix == True
 def printMatrix(matrixToPrint, roundMatrix=True):
     for row in matrixToPrint:
         for value in row:
@@ -53,9 +57,11 @@ def printMatrix(matrixToPrint, roundMatrix=True):
         print()
 
 
+# main() -> None
+# runs code to solve 5x5, 7x7 MDPs
 def main():
     actions = ['left'.upper(), 'right'.upper(), 'up'.upper(), 'down'.upper()]
-    policy, theta = 0.25, 0.00001
+    policy, theta = 0.25, 0.0000001
     specialPositions5x5 = {'A': [(0, 1), (4, 1)], 'B': [(0, 3), (2, 3)]}
     specialPositions7x7 = {'A': [(2, 1), (6, 1)], 'B': [(0, 5), (3, 5)]}
     m0 = [[0 for _ in range(5)] for _ in range(5)]
@@ -64,27 +70,23 @@ def main():
     m5x5_85 = stateValueFinder(m0, actions, policy, 0.85, theta, specialPositions5x5)
     m7x7_75 = stateValueFinder(m1, actions, policy, 0.75, theta, specialPositions7x7)
     m7x7_85 = stateValueFinder(m1, actions, policy, 0.85, theta, specialPositions7x7)
-    book = stateValueFinder(m0, actions, policy, 0.9, theta, specialPositions5x5)
 
-    print("State Value For 5x5, Gamma 0.75, Rounded To 1 Decimal Place")
+    print("State Value For 5x5, Discount Rate 0.75, Rounded To 1 Decimal Place")
     printMatrix(m5x5_75)
-    print("\nNot rounded")
+    print("\nNot Rounded")
     printMatrix(m5x5_75, roundMatrix=False)
-    print("\nState Value For 5x5, Gamma 0.85, Rounded To 1 Decimal Place")
+    print("\nState Value For 5x5, Discount Rate 0.85, Rounded To 1 Decimal Place")
     printMatrix(m5x5_85)
-    print("\nNot rounded")
+    print("\nNot Rounded")
     printMatrix(m5x5_85, roundMatrix=False)
-    print("\nState Value For 7x7, Gamma 0.75, Rounded To 1 Decimal Place")
+    print("\nState Value For 7x7, Discount Rate 0.75, Rounded To 1 Decimal Place")
     printMatrix(m7x7_75)
-    print("\nNot rounded")
+    print("\nNot Rounded")
     printMatrix(m7x7_75, roundMatrix=False)
-    print("\nState Value For 7x7, Gamma 0.85, Rounded To 1 Decimal Place")
+    print("\nState Value For 7x7, Discount Rate 0.85, Rounded To 1 Decimal Place")
     printMatrix(m7x7_85)
-    print("\nNot rounded")
+    print("\nNot Rounded")
     printMatrix(m7x7_85, roundMatrix=False)
-    print("book\n")
-    print(book)
-    printMatrix(book)
 
 
 if __name__ == "__main__":
